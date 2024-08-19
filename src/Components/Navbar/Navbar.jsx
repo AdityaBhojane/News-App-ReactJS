a0a669276e0b79f9e5bef08a28c1381aeefef82f
aditya bhojane <adityabhojane66@gmail.com>
2024-08-19 23:13:13 +0530

    first commit basic news feed added

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,85 @@
+function Navbar() {
+  return (
+    <>
+      <div className="navbar bg-base-100 relative z-10">
+        <div className="w-[80%] m-auto">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost lg:hidden"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="h-5 w-5"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h8m-8 6h16"
+                  />
+                </svg>
+              </div>
+              <ul
+                tabIndex={0}
+                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+              >
+                <li>
+                  <a>Item 1</a>
+                </li>
+                <li>
+                  <a>Parent</a>
+                  <ul className="p-2">
+                    <li>
+                      <a>Submenu 1</a>
+                    </li>
+                    <li>
+                      <a>Submenu 2</a>
+                    </li>
+                  </ul>
+                </li>
+                <li>
+                  <a>Item 3</a>
+                </li>
+              </ul>
+            </div>
+            <a className="btn btn-ghost text-2xl">News Feed</a>
+          </div>
+          <div className="navbar-center hidden lg:flex">
+            <ul className="menu menu-horizontal px-1 flex gap-5 font-bold">
+              <li>
+                <a>Sports</a>
+              </li>
+              <li>
+                <a>Bollywood</a>
+              </li>
+              <li>
+                <details>
+                  <summary>Country</summary>
+                  <ul className="p-2">
+                    <li>
+                      <a>India</a>
+                    </li>
+                    <li>
+                      <a>USA</a>
+                    </li>
+                  </ul>
+                </details>
+              </li>
+            </ul>
+          </div>
+          <div className="ml-[20%]">
+            <a className="btn btn-primary">Latest News</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Navbar;
